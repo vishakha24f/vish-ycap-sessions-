4ed09f03d372687efa9d0be7533e636e528b0fe7
vishakha24f <21071215@ycce.in>
2022-12-21 10:24:33 +0000

    addition

---
+++ addition.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("enter the value of a ");
+    scanf("%d", &a);
+    printf("enter the value of b ");
+    scanf("%d", &b);
+    c=a+b;
+    printf("the value of c is %d", c);
+    return 0;
+}
